7f0f3a820424f0ac098ecd5dab6e1d99313f2b67
Lizandra <lizandra.oliveirasouza@hotmail.com>
2020-10-29 09:40:50 -0300

    commit inicial

---
+++ Lizandra.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lizandra.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
